ab9fc80311730c057e554747e342de00906685d8
Dipanshu Agrawal <codedipanshu@gmail.com>
2024-10-28 22:25:41 +0530

    init headers (routes)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,29 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/api/v1/signup", (c) => {
+  return c.text("signup route");
+});
 
-export default app
+app.post("/api/v1/signin", (c) => {
+  return c.text("signin route");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("post a blog route");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("update a blog route");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("get a blog route");
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("get all route");
+});
+
+export default app;
