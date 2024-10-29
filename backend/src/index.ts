37cdfc885de457ab674c2b84443866fd14aba357
Dipanshu Agrawal <codedipanshu@gmail.com>
2024-10-29 22:33:08 +0530

    Signup Route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,41 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
 
+// Signup Route
 app.post("/api/v1/signup", async (c) => {
+  // Init Prisma Client
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
-  
-  return c.text("signup route");
+
+  const body = await c.req.json();
+
+  try {
+    // Create User from body
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    // Sign JWT token
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+
+    return c.json({ jwt });
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "errror while signining up" });
+  }
 });
 
 app.post("/api/v1/signin", (c) => {
