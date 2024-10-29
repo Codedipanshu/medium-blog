2f6937d3ddfc714cf1a3a676bc73f2309183e67c
Dipanshu Agrawal <codedipanshu@gmail.com>
2024-10-29 22:47:35 +0530

    Signin Route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt";
+import { sign, verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
@@ -38,8 +38,35 @@ app.post("/api/v1/signup", async (c) => {
   }
 });
 
-app.post("/api/v1/signin", (c) => {
-  return c.text("signin route");
+// Signin Route
+app.post("/api/v1/signin", async (c) => {
+  // Init Prisma Client
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  try {
+    // Find user
+    const user = await prisma.user.findUnique({
+      where: { email: body.email, password: body.password },
+    });
+
+    if (!user) {
+      c.status(403);
+      return c.json({
+        error: "user not found or email/password does not match",
+      });
+    }
+
+    // Sign JWT token
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "errror while signining in" });
+  }
 });
 
 app.post("/api/v1/blog", (c) => {
