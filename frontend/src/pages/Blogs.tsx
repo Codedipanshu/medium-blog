0c1d7fd1c1e92a888c607f88e25d54c291e9238b
Dipanshu Agrawal <codedipanshu@gmail.com>
2024-11-04 17:50:51 +0530

    blogs component

---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,52 @@
+import BlogCard from "../components/BlogCard";
+
+const Blogs = () => {
+  return (
+    <div className=" flex justify-center">
+      <div className=" max-w-2xl">
+        <BlogCard
+          authorName={"Dipanshu Agrawal"}
+          title={
+            "How an ugly single page website makes $5000 a month without affiliate markeetting"
+          }
+          content={
+            "How an ugly single page website makes $5000 a month without affiliate markeetting How an ugly single page website makes $5000 a month without affiliate markeetting How an ugly single page website makes $5000 a month without affiliate markeetting"
+          }
+          publishDate={"2nd Feb 2024"}
+        />
+        <BlogCard
+          authorName={"Dipanshu Agrawal"}
+          title={
+            "How an ugly single page website makes $5000 a month without affiliate markeetting"
+          }
+          content={
+            "How an ugly single page website makes $5000 a month without affiliate markeetting How an ugly single page website makes $5000 a month without affiliate markeetting How an ugly single page website makes $5000 a month without affiliate markeetting"
+          }
+          publishDate={"2nd Feb 2024"}
+        />
+        <BlogCard
+          authorName={"Dipanshu Agrawal"}
+          title={
+            "How an ugly single page website makes $5000 a month without affiliate markeetting"
+          }
+          content={
+            "How an ugly single page website makes $5000 a month without affiliate markeetting How an ugly single page website makes $5000 a month without affiliate markeetting How an ugly single page website makes $5000 a month without affiliate markeetting"
+          }
+          publishDate={"2nd Feb 2024"}
+        />
+        <BlogCard
+          authorName={"Dipanshu Agrawal"}
+          title={
+            "How an ugly single page website makes $5000 a month without affiliate markeetting"
+          }
+          content={
+            "How an ugly single page website makes $5000 a month without affiliate markeetting How an ugly single page website makes $5000 a month without affiliate markeetting How an ugly single page website makes $5000 a month without affiliate markeetting"
+          }
+          publishDate={"2nd Feb 2024"}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Blogs;
